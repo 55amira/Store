67ca7997695a38bfc235b0a08f020a9bf66370ea
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-04-12 13:22:58 +0200

    Impelementation Database

--- Store_Api/Program.cs
+++ Store_Api/Program.cs
@@ -1,9 +1,14 @@
 
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Data;
+
 namespace Store_Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +19,20 @@ namespace Store_Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                //options.UseSqlServer(builder.Configuration["ConnectionStrings:DefultConnection"]);
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefultConnection"));
+            });
+            builder.Services.AddScoped<IDbInitializer,Iniaitiazer>();   // Alloe DI For IDbInitializer
+
             var app = builder.Build();
+
+            #region Seeding 
+            using var scope = app.Services.CreateScope();
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>(); // Ask CLR Create Object From Iniaitiazer
+            await dbInitializer.InitializerAsync(); 
+            #endregion
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
