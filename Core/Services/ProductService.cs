7ee8d826a4f561326402b07c24c8159e18044b7d
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-04-12 15:31:03 +0200

    Services

---
+++ Core/Services/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using Services_Abstractions;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductService : IProductService
+    {
+        public ProductService( )
+        { 
+        }
+        public Task<IEnumerable<ProductResultDto>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductResultDto> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<BrandResultDto>> GetAllBrandsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BrandResultDto>> GetAlltypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
