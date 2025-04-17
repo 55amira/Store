cc1cc094138e6aa3fa794aee3cf792614fd39d18
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-04-17 18:31:39 +0200

    Build Specification Evaluator

---
+++ Infrastructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Persistence
+{
+    static class SpecificationEvaluator
+    {
+      public static IQueryable<TEntity> GetQuery<TEntity, TKey>(IQueryable<TEntity> inputQuery,ISpecifications<TEntity, TKey> spec)   where TEntity : BaseEntity<TKey>
+       {
+            var query = inputQuery;
+            if (spec.Criteria is not null)
+                query= query.Where(spec.Criteria);
+
+            query = spec.IncludeExperssions.Aggregate(query, (currentQuery, includeExpression) => currentQuery.Include(includeExpression));
+             return query;
+       }
+            
+    }               
+}
