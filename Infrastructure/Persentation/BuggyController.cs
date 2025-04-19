248276bd5ce6999bbc83d752f690376294aa4bff
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-04-19 15:09:21 +0200

    Error Types at API

---
+++ Infrastructure/Persentation/BuggyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuggyController : ControllerBase
+    {
+        [HttpGet ("NotFound")]
+        public IActionResult GetNotFoundResult ()
+        {
+            return NotFound(); // 404
+        }
+
+        [HttpGet("ServerError")]
+        public IActionResult GetServerErrorRequest()
+        {
+            throw new Exception();
+            return Ok(); 
+        }
+
+        [HttpGet("badRequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest(); // 400
+        }
+        [HttpGet("badRequest/[id]")]
+        public IActionResult GetBadRequest(int id) // validation error
+        {
+            return BadRequest(); // 400
+        }
+
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorizedRequest()
+        {
+            return Unauthorized(); // 401
+        }
+    }
+}
